b2d39891dd1f4f67ef9fd60e19f4e9fa435b2481
Takumi Sekiguchi <takumis2013@yahoo.co.jp>
2018-01-06 00:20:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusiciansSnsApp::Application.config.secret_key_base = '11bc6585983aff4942d06458b9a9f05c644effe4f34d39c4201997264d94ba23dad191bd10d7622abec538e1c05bc4635433000ae8acb134a9ad7d36b3d162b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MusiciansSnsApp::Application.config.session_store :cookie_store, key: '_MusiciansSns_App_session'
