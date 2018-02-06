43b942476e139267f3d8307020c56582912681f7
Takumi Sekiguchi <takumis2013@yahoo.co.jp>
2018-02-06 02:27:15 +0000

    Adjust Sarch's Javascripts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, :except=>[:show]
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.search(params[:search]).paginate(page: params[:page])
   end
   
   def show
     @user = User.find(params[:id])
     @favorites = @user.favorites
     @favorite = @favorites.build if signed_in?
+    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,14 @@ class User < ActiveRecord::Base
          
   include JpPrefecture
   jp_prefecture :prefecture_code, method_name: :pref
+  
+  def self.search(search) #self.でクラスメソッドとしている
+    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+      #User.where(['name LIKE ?', "%#{search}%"])
+      #User.joins(:favorites).where("favorite.artist like '%#{params[:search]}'").uniq
+      User.joins(:favorites).where(['favorites.artist LIKE ?', "%#{search}%"])
+    else
+      User.all #全て表示。
+    end
+  end
 end
